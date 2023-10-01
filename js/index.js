cb93c73f89a5fa19acb41478fd96ca2c538e1066
William Clayton <william.d.clayton1@gmail.com>
2023-10-01 18:55:42 -0500

    finished first page style and responsiveness -wc

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 
+let submit = document.querySelector('#submit-btn');
+
+let radioBtn = document.getElementsByTagName('input');
+
+let btnLabel = document.getElementsByClassName('display');
+console.log(btnLabel);
+submit.addEventListener('click', () => {
+    for (let btn of radioBtn) {
+        console.log(btn.checked);
+    }
+})
