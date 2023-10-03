d124bc4c7141158b387c4dd237a1c5334cc3aadd
William Clayton <william.d.clayton1@gmail.com>
2023-10-03 09:57:47 -0500

    finished the responsiveness of the rating page -wc

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
-
 let submit = document.querySelector('#submit-btn');
-
 let radioBtn = document.getElementsByTagName('input');
+let span = document.querySelector('span');
+let rating = document.querySelector('.rating-container');
+let results = document.querySelector('.results-container');
 
-let btnLabel = document.getElementsByClassName('display');
-console.log(btnLabel);
+results.style.display = 'none';
+
 submit.addEventListener('click', () => {
+    rating.style.display = 'none';
+    results.style.display = 'flex';
+
     for (let btn of radioBtn) {
-        console.log(btn.checked);
+        if (btn.checked) {
+            span.innerText = btn.value
+        }
     }
 })
